5fc6936a0be7f8574773d15020fae4f82c8ea184
unknown <taherbashaengineer@gmail.com>
2026-03-01 13:15:39 +0530

    feat(frontend): implement login and register pages with form handling

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {useNavigate,Link} from 'react-router'
+import '../auth.form.scss'
 
 const Login = ()=>{
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
     return (
-        <div>Login</div>
+        <main>
+            <div className="form-container">
+                <h1>Login</h1>
+
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" placeholder='Enter Email Address' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder='Enter password' />
+                    </div>
+
+                    <button className='button primary-button'>Login</button>
+                </form>
+                <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
+            </div>
+        </main>
     )
 }
 
