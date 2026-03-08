8aa19f6c925dae7f9e10ae480b5396c9e174447d
unknown <taherbashaengineer@gmail.com>
2026-03-08 13:48:17 +0530

    feat: add frontend API URL to CORS configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+        "https://careerpilot-ai-1-zs3k.onrender.com"
+    ],
     credentials: true
 }))
 
